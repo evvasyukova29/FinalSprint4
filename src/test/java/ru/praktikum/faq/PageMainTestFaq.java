969a73a94521b812ce4718ccddf3bafac41e26d8
Elena Vasyukova <evvasyukova29@yandex.ru>
2023-05-24 16:11:08 +0300

    Add: New Class with Parameters For question

--- src/test/java/ru/praktikum/faq/PageMainTestFaq.java
+++ src/test/java/ru/praktikum/faq/PageMainTestFaq.java
@@ -2,10 +2,13 @@ package ru.praktikum.faq;
 
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import ru.praktikum.makeorder.OrderPageTest;
 import ru.praktikum.pageobject.PageMain;
 
 import java.time.Duration;
@@ -17,106 +20,103 @@ public class PageMainTestFaq
 {
 
         private WebDriver driver;
+        private static By question;
+        private static By questionText;
+    public PageMainTestFaq(By question,By questionText)
+    {
+        PageMainTestFaq.question = question;
+        PageMainTestFaq.questionText = question;
+    }
 
+    @Parameterized.Parameters // параметризация
+    public static Object[][] getAnswerForQuiestions()
+    {
+        return new Object[][]
+                {
+                        {PageMain.questionOne, PageMain.questionTwoText},
+                        {PageMain.questionTwo, PageMain.questionTwoText},
+                        {PageMain.questionThree, PageMain.questionThreeText},
+                        {PageMain.questionFour, PageMain.questionFourText},
+                        {PageMain.questionFive, PageMain.questionFiveText},
+                        {PageMain.questionSix, PageMain.questionSixText},
+                        {PageMain.questionSeven, PageMain.questionSevenText},
+                        {PageMain.questionEight, PageMain.questionEightText},
+                };
+    }
+        @Before
+        public void setUp()
+        {
+            driver = new ChromeDriver();
+            driver.get(APP_URL);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.manage().window().maximize();
+            PageMain mainPage = new PageMain(driver);
+            mainPage.scrollToElement();
+            mainPage.clickCookie();
+
+        }
         @Test
         public void testQuestionOne()
         {
-            driver = new ChromeDriver();
             PageMain mainPage = new PageMain(driver);
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.get(APP_URL);
-            mainPage.scrollToElement();
-            mainPage.clickCookie();
             mainPage.clickButtonOneQuestion(PageMain.questionOne);
             assertEquals(PageMain.questionOneText, mainPage.getTextQuestionOne());  // сравнить текст
         }
 
         @Test
         public void testQuestionTwo() {
-            driver = new ChromeDriver();
             PageMain mainPage = new PageMain(driver);
-            driver.manage().window().maximize();
-            driver.get(APP_URL);
-            mainPage.scrollToElement();
-            mainPage.clickCookie();
             mainPage.clickButtonTwoQuestion(PageMain.questionTwo);
             assertEquals(PageMain.questionTwoText, mainPage.getTextQuestionTwo());  // сравнить текст
         }
 
         @Test
-        public void testQuestionThree() {
-            driver = new ChromeDriver();
+        public void testQuestionThree()
+        {
             PageMain mainPage = new PageMain(driver);
-            driver.manage().window().maximize();
-            driver.get(APP_URL);
-            mainPage.scrollToElement();
-            mainPage.clickCookie();
             mainPage.clickButtonThreeQuestion(PageMain.questionThree);
             assertEquals(PageMain.questionThreeText, mainPage.getTextQuestionThree());  // сравнить текст
         }
 
         @Test
         public void testQuestionFour() {
-            driver = new ChromeDriver();
             PageMain mainPage = new PageMain(driver);
-            driver.manage().window().maximize();
-            driver.get(APP_URL);
-            mainPage.scrollToElement();
-            mainPage.clickCookie();
             mainPage.clickButtonFourQuestion(PageMain.questionFour);
             assertEquals(PageMain.questionFourText, mainPage.getTextQuestionFour());  // сравнить текст
         }
 
         @Test
-        public void testQuestionFive() {
-            driver = new ChromeDriver();
+        public void testQuestionFive()
+        {
             PageMain mainPage = new PageMain(driver);
-            driver.manage().window().maximize();
-            driver.get(APP_URL);
-            mainPage.scrollToElement();
-            mainPage.clickCookie();
             mainPage.clickButtonFiveQuestion(PageMain.questionFive);
             assertEquals(PageMain.questionFiveText, mainPage.getTextQuestionFive());  // сравнить текст
         }
 
         @Test
         public void testQuestionSix() {
-            driver = new ChromeDriver();
             PageMain mainPage = new PageMain(driver);
-            driver.manage().window().maximize();
-            driver.get(APP_URL);
-            mainPage.scrollToElement();
-            mainPage.clickCookie();
             mainPage.clickButtonSixQuestion(PageMain.questionSix);
             assertEquals(PageMain.questionSixText, mainPage.getTextQuestionSix());  // сравнить текст
         }
 
         @Test
         public void testQuestionSeven() {
-            driver = new ChromeDriver();
             PageMain mainPage = new PageMain(driver);
-            driver.manage().window().maximize();
-            driver.get(APP_URL);
-            mainPage.scrollToElement();
-            mainPage.clickCookie();
             mainPage.clickButtonSevenQuestion(PageMain.questionSeven);
             assertEquals(PageMain.questionSevenText, mainPage.getTextQuestionSeven());  // сравнить текст
         }
 
         @Test
         public void testQuestionEight() {
-            driver = new ChromeDriver();
             PageMain mainPage = new PageMain(driver);
-            driver.manage().window().maximize();
-            driver.get(APP_URL);
-            mainPage.scrollToElement();
-            mainPage.clickCookie();
             mainPage.clickButtonEightQuestion(PageMain.questionEight);
             assertEquals(PageMain.questionEightText, mainPage.getTextQuestionEight());  // сравнить текст
         }
 
         @After
-        public void tearDown() {
+        public void tearDown()
+        {
             driver.quit();
         }
     }
